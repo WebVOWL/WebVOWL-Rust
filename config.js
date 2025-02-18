aa7cc454260c32937e3ea2bd9db123082bb2e271
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-02-18 15:55:23 +0100

    Export the path object for use by other modules

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
-let paths = {
-    srcPath: "src/main",
-    webappPath: `${srcPath}/webapp`,
-    frontendPath: `${webappPath}/frontend`,
-    backendPath: `${webappPath}/backend`,
-    testPath: `${srcPath}/test/frontend`,
-    deployPath: "target/deploy",
-    nodeModulePath: "/target/node/node_modules/npm/node_modules"
+module.exports.path_func = new function () {
+    this.srcPath = "src/main";
+    this.webappPath = `${this.srcPath}/webapp`;
+    this.frontendPath = `${this.webappPath}/frontend`;
+    this.backendPath = `${this.webappPath}/backend`;
+    this.testPath = `${this.srcPath}/test/frontend`;
+    this.deployPath = "target/deploy";
+    this.deployZipPath = `${this.deployPath}/compressed`
+    this.nodeModulePath = "node_modules";
 };
