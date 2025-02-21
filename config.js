f2a04fe4cde73382ddc0fc0a26763a814068f6b7
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-02-21 15:19:34 +0100

    Add paths for rust development

--- config.js
+++ config.js
@@ -1,10 +1,19 @@
 module.exports.path_func = new function () {
+    // General stuff
     this.srcPath = "src/main";
+
+    // Deploy
+    this.deployPath = "target/deploy";
+    this.deployZipPath = `${this.deployPath}/compressed`
+
+    // Rust
+    this.rustPath = `${this.srcPath}/rust`;
+    this.pgkPath = `${this.deployPath}/pgk`
+
+    // JS/CSS/HTML
     this.webappPath = `${this.srcPath}/webapp`;
     this.frontendPath = `${this.webappPath}/frontend`;
     this.backendPath = `${this.webappPath}/backend`;
     this.testPath = `${this.srcPath}/test/frontend`;
-    this.deployPath = "target/deploy";
-    this.deployZipPath = `${this.deployPath}/compressed`
     this.nodeModulePath = "node_modules";
 };
