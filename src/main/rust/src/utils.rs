6a359c3a54052c61c3c629e52bf9e653bae43c3e
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-03-05 15:19:50 +0100

    Ensure panic hook is always called

--- src/main/rust/src/utils.rs
+++ src/main/rust/src/utils.rs
@@ -5,7 +5,6 @@ pub fn set_panic_hook() {
     //
     // For more details see
     // https://github.com/rustwasm/console_error_panic_hook#readme
-    #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 
     // Many browsers only capture the top 10 frames of a stack trace.
