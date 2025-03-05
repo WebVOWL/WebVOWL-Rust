93dfe7e642366d2b91c47ece2998fc7fa74c40fd
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-03-05 15:19:01 +0100

    Disable threads until webpack bundle bug is resolved

--- src/main/webapp/test/test.js
+++ src/main/webapp/test/test.js
@@ -1,20 +1,13 @@
-// import { path_func } from '../../../../config.js';
-// import init, { initThreadPool, run } from `../${paths.pkgPath}/index.js`;
-
-// const paths = path_func
-
-// CHECKOUT: https://webpack.js.org/guides/code-splitting/
-
-import init, { initThreadPool, run } from "../../rust/pkg/index.js";
+import init, { run } from "../../rust/pkg/index.js";
 
 // Regular wasm-bindgen initialization.
 await init();
 
 // Thread pool initialization with the given number of threads
 // (pass `navigator.hardwareConcurrency` if you want to use all cores).
-await initThreadPool(navigator.hardwareConcurrency);
+// await initThreadPool(navigator.hardwareConcurrency);
 
 
-window.addEventListener("load", () => {
-    run()
-})
+console.log("Starting")
+run()
+console.log("Code run")
