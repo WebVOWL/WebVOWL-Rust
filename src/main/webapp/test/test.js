592884225b4cfd905a28e64c7f1d5da6dd95662a
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-03-06 16:11:07 +0100

    Simple file loader test

--- src/main/webapp/test/test.js
+++ src/main/webapp/test/test.js
@@ -3,11 +3,22 @@ import init, { run } from "../../rust/pkg/index.js";
 // Regular wasm-bindgen initialization.
 await init();
 
+// Wasm workers: https://github.com/sgasse/wasm_worker_interaction
+// Simplify web workers: https://github.com/GoogleChromeLabs/comlink
+
 // Thread pool initialization with the given number of threads
 // (pass `navigator.hardwareConcurrency` if you want to use all cores).
 // await initThreadPool(navigator.hardwareConcurrency);
 
 
 console.log("Starting")
-run()
-console.log("Code run")
+var input = document.createElement('input');
+input.type = 'file';
+
+input.onchange = e => {
+    var file = e.target.files[0];
+    run(file)
+    console.log("Code run")
+}
+var canvas_section = document.getElementById("canvasArea")
+canvas_section.insertBefore(input, canvas_section.firstChild)
